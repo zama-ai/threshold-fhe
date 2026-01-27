0913898df89ac1aa07d1d6b9f626f48f6c6aac8b
Titouan Tanguy <titouan.tanguy@zama.ai>
2026-01-27 11:00:09 +0100

    chore: update to examples

--- examples/distributed_decryption.rs
+++ examples/distributed_decryption.rs
@@ -2,7 +2,7 @@
 //! you first need to add it as a dependency in your `Cargo.toml`:
 //!
 //! ```
-//! threshold_fhe = { git = "https://github.com/zama-ai/threshold-fhe.git" }
+//! threshold_fhe = { git = "https://github.com/zama-ai/kms-core.git" }
 //! ```
 //!
 //! This is an example where we setup a testing runtime that runs 4 parties on the same machine.
@@ -14,60 +14,55 @@ use tfhe::{set_server_key, FheUint8};
 use threshold_fhe::{
     algebra::{galois_rings::degree_4::ResiduePolyF4Z64, structure_traits::Ring},
     execution::{
-        endpoints::decryption::{threshold_decrypt64, DecryptionMode},
-        runtime::test_runtime::{generate_fixed_identities, DistributedTestRuntime},
+        endpoints::decryption::{threshold_decrypt64, DecryptionMode, RadixOrBoolCiphertext},
+        runtime::test_runtime::{generate_fixed_roles, DistributedTestRuntime},
         tfhe_internals::{
-            parameters::BC_PARAMS_SAM_SNS,
-            test_feature::{gen_key_set, keygen_all_party_shares, KeySet},
+            parameters::BC_PARAMS_SNS,
+            test_feature::{gen_key_set, keygen_all_party_shares_from_keyset, KeySet},
             utils::expanded_encrypt,
         },
     },
     networking::NetworkMode,
 };
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let num_parties = 4;
     let threshold = 1;
     let mut rng = AesRng::from_entropy();
 
     // Generate the keys normally, we'll secret share them later.
-    let keyset: KeySet = gen_key_set(BC_PARAMS_SAM_SNS, &mut rng);
+    let keyset: KeySet = gen_key_set(BC_PARAMS_SNS, tfhe::Tag::default(), &mut rng);
     set_server_key(keyset.public_keys.server_key.clone());
 
-    let lwe_secret_key = keyset.get_raw_lwe_client_key();
-    let glwe_secret_key = keyset.get_raw_glwe_client_key();
-    let glwe_secret_key_sns_as_lwe = keyset.sns_secret_key.key.clone();
-    let params = keyset.sns_secret_key.params;
-    let key_shares = keygen_all_party_shares(
-        lwe_secret_key,
-        glwe_secret_key,
-        glwe_secret_key_sns_as_lwe,
-        params,
-        &mut rng,
-        num_parties,
-        threshold,
-    )
-    .unwrap();
+    let params = keyset.get_cpu_params().unwrap();
+    let key_shares =
+        keygen_all_party_shares_from_keyset(&keyset, params, &mut rng, num_parties, threshold)
+            .unwrap();
 
     // Encrypt a message and extract the raw ciphertexts.
     let message = rng.gen::<u8>();
     let ct: FheUint8 = expanded_encrypt(&keyset.public_keys.public_key, message, 8).unwrap();
-    let (raw_ct, _id, _tag) = ct.into_raw_parts();
+    let (raw_ct, _id, _tag, _rerand_metadata) = ct.into_raw_parts();
+    let raw_ct = RadixOrBoolCiphertext::Radix(raw_ct);
 
     // Setup the test runtime.
     // Using Sync because threshold_decrypt64 encompasses both online and offline
-    let identities = generate_fixed_identities(num_parties);
+    let roles = generate_fixed_roles(num_parties);
     let mut runtime = DistributedTestRuntime::<
         ResiduePolyF4Z64,
+        _,
         { ResiduePolyF4Z64::EXTENSION_DEGREE },
-    >::new(identities.clone(), threshold as u8, NetworkMode::Sync, None);
+    >::new(roles.clone(), threshold as u8, NetworkMode::Sync, None);
 
-    let keyset_ck = Arc::new(keyset.public_keys.sns_key.clone().unwrap());
-    runtime.conversion_keys = Some(keyset_ck.clone());
+    let server_key = Arc::new(keyset.public_keys.server_key.clone());
+    runtime.setup_server_key(server_key);
     runtime.setup_sks(key_shares);
 
     // Perform distributed decryption.
-    let result = threshold_decrypt64(&runtime, &raw_ct, DecryptionMode::NoiseFloodSmall).unwrap();
+    let result = threshold_decrypt64(&runtime, &raw_ct, DecryptionMode::NoiseFloodSmall)
+        .await
+        .unwrap();
 
     for (_, v) in result {
         assert_eq!(v.0 as u8, message);
